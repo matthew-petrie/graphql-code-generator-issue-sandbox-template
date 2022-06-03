0343b87414b62f3c52c1fc05935c385e013d689d
Matthew Petrie <dev@matthewpetrie.co.uk>
2022-06-03 13:28:51 +0100

    feat: reproduciton of type error for URQL GraphCache

--- types.ts
+++ types.ts
@@ -1,1 +1,72 @@
+import { Resolver as GraphCacheResolver, UpdateResolver as GraphCacheUpdateResolver, OptimisticMutationResolver as GraphCacheOptimisticMutationResolver, StorageAdapter as GraphCacheStorageAdapter } from '@urql/exchange-graphcache';
+import { IntrospectionData } from '@urql/exchange-graphcache/dist/types/ast';
+export type Maybe<T> = T | null;
+export type InputMaybe<T> = Maybe<T>;
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: string;
+  String: string;
+  Boolean: boolean;
+  Int: number;
+  Float: number;
+};
 
+export type Query = {
+  __typename?: 'Query';
+  user: User;
+};
+
+
+export type QueryUserArgs = {
+  id: Scalars['ID'];
+};
+
+export type User = {
+  __typename?: 'User';
+  email: Scalars['String'];
+  id: Scalars['ID'];
+  username: Scalars['String'];
+};
+
+export type UserQueryVariables = Exact<{ [key: string]: never; }>;
+
+
+export type UserQuery = { __typename?: 'Query', user: { __typename?: 'User', id: string, username: string, email: string } };
+
+export type WithTypename<T extends { __typename?: any }> = T & { __typename: NonNullable<T['__typename']> };
+
+export type GraphCacheKeysConfig = {
+  User?: (data: WithTypename<User>) => null | string
+}
+
+export type GraphCacheResolvers = {
+  Query?: {
+    user?: GraphCacheResolver<WithTypename<Query>, QueryUserArgs, WithTypename<User> | string>
+  },
+  User?: {
+    id?: GraphCacheResolver<WithTypename<User>, Record<string, never>, Scalars['ID'] | string>,
+    username?: GraphCacheResolver<WithTypename<User>, Record<string, never>, Scalars['String'] | string>,
+    email?: GraphCacheResolver<WithTypename<User>, Record<string, never>, Scalars['String'] | string>
+  }
+};
+
+export type GraphCacheOptimisticUpdaters = {
+  {}
+};
+
+export type GraphCacheUpdaters = {
+  Mutation?: {},
+  Subscription?: {},
+};
+
+export type GraphCacheConfig = {
+  schema?: IntrospectionData,
+  updates?: GraphCacheUpdaters,
+  keys?: GraphCacheKeysConfig,
+  optimistic?: GraphCacheOptimisticUpdaters,
+  resolvers?: GraphCacheResolvers,
+  storage?: GraphCacheStorageAdapter
+};
